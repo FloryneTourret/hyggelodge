ed8294bd746b612345209d1041814f862fbf6aa3
Floryne Tourret <floryne.tourret.pro@gmail.com>
2022-04-13 09:54:45 +0200

    livechat

--- footer.php
+++ footer.php
@@ -45,8 +45,10 @@ $logo_white = get_theme_mod('hyggelodge_logo_white');
                 <li><a href="<?php echo esc_url(home_url('/mentions-legales/')); ?>">Mentions légales</a></li>
             </ul>
             <ul>
-                <li><a href=""><ion-icon name="logo-instagram"></ion-icon></a>
-                <a href=""><ion-icon name="logo-facebook"></ion-icon></a></li>
+                <li>
+                    <a href="https://www.instagram.com/hyggelodgeannecy/"><ion-icon name="logo-instagram"></ion-icon></a>
+                    <a href="https://www.facebook.com/hyggelodge"><ion-icon name="logo-facebook"></ion-icon></a>
+            </li>
             </ul>
             
             
@@ -80,6 +82,25 @@ $logo_white = get_theme_mod('hyggelodge_logo_white');
         });
     });
 </script>
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+<script type="text/javascript">
+  window.__lc = window.__lc || {};
+  window.__lc.license = 14018700;
+  ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)};
+  var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){
+  i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},
+  get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");
+  return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){
+  var n=t.createElement("script");
+  n.async=!0,n.type="text/javascript",
+  n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};
+  !n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+</script>
+<noscript>
+<a href="https://www.livechatinc.com/chat-with/14018700/" rel="nofollow">Chat with us</a>,
+powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
+</noscript>
+<!-- End of LiveChat code -->
 
 <?php wp_footer(); ?>
 </body>
